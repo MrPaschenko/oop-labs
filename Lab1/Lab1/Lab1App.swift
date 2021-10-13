c53739d0721ee29f780d802a0729a9ddaa6cc9c2
Dmytro Pashchenko <paschenkod2@gmail.com>
2021-10-13 14:08:32 +0300

    lab1

---
+++ Lab1/Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by Дмитрий Пащенко on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1/Lab1/work1.swift
@@ -0,0 +1,20 @@
+//
+//  work1.swift
+//  Lab1
+//
+//  Created by Дмитрий Пащенко on 15.09.2021.
+//
+
+import SwiftUI
+
+struct work1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct work1_Previews: PreviewProvider {
+    static var previews: some View {
+        work1()
+    }
+}
